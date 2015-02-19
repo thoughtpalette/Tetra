23cacd1f9351c20add0a2e21aa38ada1327edde9
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-02-19 16:32:08 -0600

    added svg, changed port to 1337

--- source/scripts/project/controllers/Site.js
+++ source/scripts/project/controllers/Site.js
@@ -12,56 +12,9 @@ angular.module( "vokal.controllers" )
         $scope.fabricItems = [];
         $scope.pageTiers = [{tier: 1 }, {tier: 2 }, {tier: 3 } ];
 
-        $scope.pageOptions = [
-            {
-                id: 1,
-                type: "Landing Page"
-            },
-            {
-                id: 2,
-                type: "Site Level"
-            },
-            {
-                id: 3,
-                type: "Form/Dynamic Component"
-            },
-            {
-                id: 4,
-                type: "To be produced now"
-            },
-            {
-                id: 5,
-                type: "Title not yet decided"
-            },
-            {
-                id: 6,
-                type: "To be added at later time"
-            },
-            {
-                id: 7,
-                type: "Link opens new window or leaves site"
-            },
-            {
-                id: 8,
-                type: "Content alts on same page"
-            },
-            {
-                id: 9,
-                type: "Content is part of same page"
-            },
-            {
-                id: 10,
-                type: "Contents to be removed"
-            },
-            {
-                id: 11,
-                type: "Email Link"
-            },
-            {
-                id: 12,
-                type: "Download"
-            }
-        ];
+        $scope.isPageMode = true;
+
+        $scope.pageOptions = [{id: 1, type: "Landing Page"}, {id: 2, type: "Site Level"}, {id: 3, type: "Form/Dynamic Component"}, {id: 4, type: "To be produced now"}, {id: 5, type: "Title not yet decided"}, {id: 6, type: "To be added at later time"}, {id: 7, type: "Link opens new window or leaves site"}, {id: 8, type: "Content alts on same page"}, {id: 9, type: "Content is part of same page"}, {id: 10, type: "Contents to be removed"}, {id: 11, type: "Email Link"}, {id: 12, type: "Download"} ];
 
         // Start up
         $scope.canvas.setWidth( $( ".main-container" ).width() - 100 );
@@ -79,6 +32,18 @@ angular.module( "vokal.controllers" )
                 });
         };
         img.src = "/build/images/legend.png";
+
+        $scope.enterDrawingMode = function ()
+        {
+            $scope.isPageMode = false;
+            $scope.canvas.isDrawingMode = true;
+        };
+
+        $scope.enterPageMode = function ()
+        {
+            $scope.canvas.isDrawingMode = false;
+            $scope.isPageMode = true;
+        };
 
         $scope.addToCanvas = function ( title, type, tier )
         {
@@ -113,36 +78,62 @@ angular.module( "vokal.controllers" )
 
             switch (type.type) {
                 case "Landing Page":
-                    var r = new fabric.Rect({
-                        width: tierWidth,
-                        height: tierHeight,
-                        fill: "#818385"
-                      });
+                    // var r = new fabric.Rect({
+                    //     width: tierWidth,
+                    //     height: tierHeight,
+                    //     fill: "#818385"
+                    //   });
 
+                    // var r = fabric.loadSVGFromURL('/build/images/svg/landing.svg', function (objects) {
+                    //      $scope.canvas.add.apply($scope.canvas, objects);
+                    //      $scope.canvas.renderAll();
+                    // });
 
-                    // create a rectangle object
-                    var t = new fabric.IText(title, {
-                      fontFamily: "Helvetica",
-                      fill: "#818385",
-                      fontSize: 12,
-                      top : 10,
-                      left: 35
+                    var group = [];
+
+                    fabric.loadSVGFromURL("/build/images/svg/landing.svg",function(objects,options) {
+
+                    var loadedObjects = new fabric.Group(group);
+
+                    loadedObjects.set({
+                            left: 100,
+                            top: 100,
+                            width:175,
+                            height:175
+                    });
+
+                    $scope.canvas.add(loadedObjects);
+                    $scope.canvas.renderAll();
+
+                    },function(item, object) {
+                            object.set('id',item.getAttribute('id'));
+                            group.push(object);
                     });
 
 
-                    var group = new fabric.Group([ r, t ], {
-                      left: 100,
-                      top: 100,
-                      lockScalingX: true,
-                      lockScalingY: true,
-                      hasRotatingPoint: false,
-                      transparentCorners: false,
-                      cornerSize: 7,
-                      stroke: "#000000",
-                      textAlign: "center"
-                    });
+                    // // create a rectangle object
+                    // var t = new fabric.IText(title, {
+                    //   fontFamily: "Helvetica",
+                    //   fill: "#818385",
+                    //   fontSize: 12,
+                    //   top : 10,
+                    //   left: 35
+                    // });
 
-                    $scope.canvas.add(group);
+
+                    // var group = new fabric.Group([ r, t ], {
+                    //   left: 100,
+                    //   top: 100,
+                    //   lockScalingX: true,
+                    //   lockScalingY: true,
+                    //   hasRotatingPoint: false,
+                    //   transparentCorners: false,
+                    //   cornerSize: 7,
+                    //   stroke: "#000000",
+                    //   textAlign: "center"
+                    // });
+
+                    // $scope.canvas.add(group);
                 break;
                 case "Site Level":
 
