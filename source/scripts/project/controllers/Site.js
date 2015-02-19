5899ccc2254bd2cd6a84214f05683abf3fa1127d
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-02-19 13:51:43 -0600

    Added legend image to bg

--- source/scripts/project/controllers/Site.js
+++ source/scripts/project/controllers/Site.js
@@ -11,9 +11,21 @@ angular.module( "vokal.controllers" )
         $scope.canvas = new fabric.Canvas('main');
         $scope.fabricItems = [];
 
+        // Start up
         $scope.canvas.setWidth( $( ".main-container" ).width() - 100 );
         $scope.canvas.setHeight( $( ".main-container" ).height() - 100 );
         $scope.canvas.calcOffset();
+
+        var img = new Image();
+        img.onload = function(){
+        $scope.canvas.setBackgroundImage(img.src, $scope.canvas.renderAll.bind($scope.canvas), {
+                    originX: 'left',
+                    originY: 'top',
+                    left: 232,
+                    top: 0
+                });
+        };
+        img.src = "/build/images/legend.png";
 
         $scope.addToCanvas = function ( title, type )
         {
