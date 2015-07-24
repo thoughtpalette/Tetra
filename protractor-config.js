2f35e1a6d08329a0cbc129645e022c79fc5d534e
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-07-24 11:00:29 -0500

    Added protractor, hurhurhur

--- protractor-config.js
+++ protractor-config.js
@@ -1,40 +1,30 @@
+// Protractor config file
+
 exports.config = {
 
-    // Protractor defaults to using the local .jar file if no seleniumAddress is given
-    // Leave seleniumServerJar commented out to default to the correct version url
-    // seleniumServerJar: "node_modules/protractor/selenium/selenium-server-standalone",
+    // The address of a running selenium server.
+    seleniumAddress: "http://localhost:4444/wd/hub",
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-        "browserName": "firefox" // or chrome or phantomjs
+        "browserName": "chrome"
     },
-
-    // Prevents a full selenium start-up, much quicker, but only works with Chrome and Firefox
     directConnect: true,
-    baseUrl: 'http://localhost:3000',
-    rootElement: 'html',
+    baseUrl: "http://localhost:3000",
 
     // Spec patterns are relative to the current working directly when protractor is called.
-    specs: [
-        "tests/*.js",
-        "!tests/*.spec.js"
-    ],
+    specs: [ "tests/*.js" ],
+
+    onPrepare: function () {
+        require( "jasmine-reporters" );
+        jasmine.getEnv().addReporter( new jasmine.JUnitXmlReporter( "coverage/net", true, true ) );
+    },
 
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
-        // If true, display spec names.
-        isVerbose: true,
-        // If true, print colors to the terminal.
         showColors: true,
-        // If true, include stack traces in failures.
-        includeStackTrace: true,
-        // Default time to wait in ms before a test fails.
-        defaultTimeoutInterval: 30000
-    },
-
-    onPrepare: function ()
-    {
-        require( "jasmine-reporters" );
-        jasmine.getEnv().addReporter( new jasmine.JUnitXmlReporter( "coverage/net", true, true ) );
+        defaultTimeoutInterval: 30000,
+        isVerbose: true
     }
+
 };
